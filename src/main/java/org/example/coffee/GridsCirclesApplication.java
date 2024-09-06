1b5127b96a6541320b2180073e5bd426735ea0b3
ayoung-dev <dkdudab@hanmail.net>
2024-09-06 15:59:31 +0900

    project setting

---
+++ src/main/java/org/example/coffee/GridsCirclesApplication.java
@@ -0,0 +1,13 @@
+package org.example.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridsCirclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridsCirclesApplication.class, args);
+    }
+
+}
