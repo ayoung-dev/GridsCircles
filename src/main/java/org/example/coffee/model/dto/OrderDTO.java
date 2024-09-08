c0c862e31f8d40507db13598cb57bac8ce84f335
ayoung-dev <dkdudab@hanmail.net>
2024-09-09 01:24:51 +0900

    feat: 주문하기 - 주문 생성 시 orders 테이블 추가

--- src/main/java/org/example/coffee/model/dto/OrderDTO.java
+++ src/main/java/org/example/coffee/model/dto/OrderDTO.java
@@ -1,5 +1,82 @@
 package org.example.coffee.model.dto;
 
-public class OrderDTO {
+import java.time.LocalDateTime;
 
+/**
+ * 주문 DTO 클래스
+ */
+
+public class OrderDTO {
+    private String orderId;
+    private String email;
+    private String address;
+    private String postcode;
+    private String orderStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public OrderDTO() {}
+
+    public OrderDTO(String email, String address, String postcode, String orderStatus) {
+        this.email = email;
+        this.address = address;
+        this.postcode = postcode;
+        this.orderStatus = orderStatus;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
--- src/main/java/org/example/coffee/model/service/OrderService.java
+++ src/main/java/org/example/coffee/model/service/OrderService.java
@@ -1,4 +1,18 @@
 package org.example.coffee.model.service;
 
+import org.example.coffee.model.dto.OrderDTO;
+import org.example.coffee.model.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
 public class OrderService {
+    @Autowired
+    private OrderRepository repo;
+
+    public int addOrder(OrderDTO orderDTO) throws SQLException {
+        return repo.insertOrder(orderDTO);
+    }
 }
